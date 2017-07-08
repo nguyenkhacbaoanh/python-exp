4d0396b1806450839e74beaa977757fcb5a1b80c
supig <khacbaoanh.nguyen@gmail.com>
2017-07-08 08:36:40 +0200

    import module json and data of json

--- read_json/read_json.py
+++ read_json/read_json.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
 characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
