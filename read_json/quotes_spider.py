03bc8a2ef046a50f61b824973cd115af225a2420
supig <khacbaoanh.nguyen@gmail.com>
2017-07-08 10:59:53 +0200

    add quotes_sprider function to import data quotes

---
+++ read_json/quotes_spider.py
@@ -0,0 +1,24 @@
+import scrapy
+from scrapy.selector import Selector
+
+class BlogSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = ['http://www.babelio.com/auteur/Frederic-Dard/7187/citations']
+
+    def parse(self, response):
+        for title in response.css('div.post_con div.text.row div'):
+            yield {'quote': title.css('div ::text').extract_first()}
+
+        next_pages = response.css('div.pagination.row > a').extract()
+        for index, page in enumerate(next_pages):
+            if 'class="active"' in page:
+                n_page = next_pages[index + 1]
+                next_page = Selector(text=n_page).xpath('//a/@href').extract()
+                next_page_url = next_page[0]
+                if index == (len(next_pages) - 1):
+                    next_page = False
+
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+
+# scrapy runspider read_json/quotes_spider.py -o read_json/quotes.json
